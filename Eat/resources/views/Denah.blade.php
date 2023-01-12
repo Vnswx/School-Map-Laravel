ea8955f322e00c77d84e3ac1df261d5b08ff8a72
Vnswx <dexieskull@gmail.com>
2023-01-12 13:45:50 +0700

    insert laravel

--- Eat/resources/views/Denah.blade.php
+++ Eat/resources/views/Denah.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/one.css">
+    <link rel="stylesheet" href="{{ asset('forcss/css/one.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,63 +24,61 @@
 </head>
 <body>
     <div class="sub">
-        <img src="img/logo-tb.png">
+        <img src="{{ asset('forcss/img/logo-tb.png') }}">
         <p> SMK TARUNA BHAKTI</p>
     </div>
     <div class="nav">
         <div class="nao">
             <h1>Gedung Teori</h1>
         <h2>Lantai 1</h2>
-        <button>Ruang 1</button>
-        <button>Ruang 2</button>
-        <button>Ruang 3</button>
-        <button>Ruang 4</button>
+        <a href="welcome"><button>Ruang 1</button></a>
+        <a href="#"><button>Ruang 2</button></a>
+        <a href="#"><button>Ruang 3</button></a>
+        <a href="#"><button>Ruang 4</button></a>
         <h2>Lantai 2</h2>
-        <button>Ruang 5</button>
-        <button>Ruang 6</button>
-        <button>Ruang 7</button>
-        <button>Ruang 8</button>
+        <a href=""><button>Ruang 5</button></a>
+        <a href=""><button>Ruang 6</button></a>
+        <a href=""><button>Ruang 7</button></a>
+        <a href=""><button>Ruang 8</button></a>
         <h2>Lantai 3</h2>
-        <button>Ruang 9</button>
-        <button>Ruang 10</button>
-        <button>Ruang 11</button>
-        <button>Ruang 12</button>
+        <a href=""><button>Ruang 9</button></a>
+        <a href=""><button>Ruang 10</button></a>
+        <a href=""><button>Ruang 11</button></a>
+        <a href=""><button>Ruang 12</button></a>
         <h2> Lantai 4</h2>
-        <button>Ruang 13</button>
-        <button>Ruang 14</button>
-        <button>Ruang 15</button>
-        <button>Ruang 16</button>
+        <a href=""><button>Ruang 13</button></a>
+        <a href=""><button>Ruang 14</button></a>
+        <a href=""><button>Ruang 15</button></a>
+        <a href=""><button>Ruang 16</button></a>
             <h1>Gedung Lab</h1>
         <h2>Lantai 1</h2>
-        <button>Lab BRF R28</button>
-        <button>Tata Usaha</button>
-        <button>Sarpras</button>
+        <a href=""><button>Lab BRF R28</button></a>
+        <a href=""><button>Tata Usaha</button></a>
+        <a href=""><button>Sarpras</button></a>
         <h2>Lantai 2</h2>
-        <button>Ruang 24</button>
-        <button>Ruang 25</button>
-        <button>Data Center TJKT</button>
-        <button>Ruang BK</button>
+        <a href=""><button>Ruang 24</button></a>
+        <a href=""><button>Ruang 25</button></a>
+        <a href=""><button>Data Center TJKT</button></a>
+        <a href=""><button>Ruang BK</button></a>
         <h2>Lantai 3</h2>
-        <button>Ruang 21</button>
-        <button>Ruang 22</button>
-        <button>Ruang 23</button>
-        <button>Ruang MPK/OSIS</button>
+        <a href=""><button>Ruang 21</button></a>
+        <a href=""><button>Ruang 22</button></a>
+        <a href=""><button>Ruang 23</button></a>
+        <a href=""><button>Ruang MPK/OSIS</button></a>
         <h2>Lantai 4</h2>
-        <button>Ruang 17</button>
-        <button>Ruang 18</button>
-        <button>Ruang 19</button>
-        <button>Ruang 20</button>
+        <a href=""><button>Ruang 17</button></a>
+        <a href=""><button>Ruang 18</button></a>
+        <a href=""><button>Ruang 19</button></a>
+        <a href=""><button>Ruang 20</button></a>
             <h1>Gedung RPS</h1>
         <h2>Lantai 1</h2>
-        <button>Ruang 29</button>
-        <button>Ruang 30</button>
-        <button>RPS TE 1</button>
-        <button>Studio BRF</button>
+        <a href=""><button>Ruang 29</button></a>
+        <a href=""><button>Ruang 30</button></a>
+        <a href=""><button>RPS TE 1</button></a>
+        <a href=""><button>Studio BRF</button></a>
         <h2>Lantai 2</h2>
-        <button>RPS TE 2</button>
-        <button>Data Center Animasi</button>
-        <p><a href="#">RPS TE 2</a></p>
-        <p><span><a href="#">Data Center Animasi</a></span> </p>
+        <a href=""><button>RPS TE 2</button></a>
+        <a href=""><button>Data Center Animasi</button></a>
         </div>
     </div>  
     <div class="sda"></div>
@@ -88,7 +86,7 @@
     <div class="asd">
         <p>Lab Cobaan</p>
             <div class="sad">
-                <model-viewer src="Kelas.gltf" alt="Denah" shadow-intensity="2" camera-controls auto-rotate ar>
+                <model-viewer src="{{ asset('forcss/3d/Kelas.gltf') }}" alt="Denah" shadow-intensity="2" camera-controls auto-rotate ar>
                     <button id="hotspotButton"
                     slot="hotspot-one"
                     data-position="..." data-normal="...">OOMaga</button>
@@ -98,7 +96,7 @@
     </div>
     <div class="minimap">
         <video controls>
-            <source src="/Video/Earth.mp4" type="video/mp4  ">
+            <source src="{{ asset('forcss/video/Earth.mp4') }}" type="video/mp4  ">
         </video>
         <div class="sdaa"></div>   
     </div>
