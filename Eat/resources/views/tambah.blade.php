bb93654e1412cf008577421d831c88109c432f1c
Vnswx <dexieskull@gmail.com>
2023-01-24 13:54:03 +0700

    new

---
+++ Eat/resources/views/tambah.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('forcss/css/tambah.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h4 class="y">Lantai 2</h4>
+        <h4 class="a">Gedung Teori</h4>
+        <hr>
+        <form action="#">
+            <label for="1" class="c">Wali Kelas:</label>
+            <br>
+            <br>
+            <input type="text" name="1" id="1" placeholder="Wali Kelas">
+            <br>
+            <br>
+            <br>
+            <label for="2" class="v">Kelas:</label>
+            <br>
+            <br>
+            <input type="text" name="2" id="2" placeholder="Kelas">
+            <br>
+            <br>
+            <br>
+            <label for="3" class="b">Jam Mulai:</label>
+            <br>
+            <br>
+            <input type="number" id="3" name="3" placeholder="Jam Mulai">
+            <br>
+            <br>
+            <br>
+            <label for="3" class="n">Jam Akhir:</label>
+            <br>
+            <br>
+            <input type="number" id="4" name="4" placeholder="Jam Akhir">
+        </form>
+    </div>
+</body>
+</html>
