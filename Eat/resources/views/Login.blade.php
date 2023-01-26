5fb6b3abee2d0edc8f6fda79ac10ca3620fd3263
Vnswx <dexieskull@gmail.com>
2023-01-26 20:52:52 +0700

    New login

--- Eat/resources/views/Login.blade.php
+++ Eat/resources/views/Login.blade.php
@@ -5,9 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{ asset('forcss/css/login.css') }}">
 </head>
 <body>
-    
+    <div class="container">
+        <form action="">
+            <label class="l1" for="effect-7">Email:</label>
+            <div class="col-3">
+                <input class="effect-7" type="email" placeholder="Email">
+                  <span class="focus-border">
+                    <i></i>
+                  </span>
+              </div>
+              <label class="l2" for="effectx-7">Password:</label>
+              <div class="col-4">
+                <input class="effect-7" id="os" type="password" placeholder="password">
+                  <span class="focus-border">
+                    <i></i>
+                  </span>
+              </div>
+        </form>
+    </div>
 </body>
 </html>
